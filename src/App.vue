6f4fe57e5cd20f0502340d013e0b8cd104529ca2
pietrocruciata <cruciata9748@gmail.com>
2024-06-12 16:25:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script >
+import AppMain from './components/AppMain.vue'
+
+export default {
+  components: {
+    AppMain
+  },
+  data() {
+    return {
+      message: 'I MIEI PROGETTI'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container p-5">
+    <h1>{{ message  }}</h1>
+
+  </div>
+<AppMain />
+</template>
+
+<style>
+
+</style>
